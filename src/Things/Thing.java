fe65e3dc7aba6b7e46ca49bd379ea96b6598041a
AlexanderBotnari <63365662+AlexanderBotnari@users.noreply.github.com>
2020-05-22 08:18:03 -0700

    Update Thing.java

--- src/Things/Thing.java
+++ src/Things/Thing.java
@@ -2,6 +2,6 @@ package Things;
 
 public abstract class Thing {
 
-	public abstract void setName(String name);
+    public abstract void setName(String name);
     public abstract String getName();
 }
