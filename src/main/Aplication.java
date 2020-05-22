6ea384de8a1e9699eec187aeb512c3147cc778dd
AlexanderBotnari <63365662+AlexanderBotnari@users.noreply.github.com>
2020-05-22 08:17:07 -0700

    Update Aplication.java

--- src/main/Aplication.java
+++ src/main/Aplication.java
@@ -10,27 +10,26 @@ public class Aplication {
 	public static void main(String[] args){
 	    		
 	    
-			Cabinet cabinet = new Cabinet();
-			cabinet.put("topShelf", new Phone("Apple Iphone 11"));
-		    cabinet.put("middleShelf", new Book("Book: Programming in Java for beginners"));
-			cabinet.put("bottomShelf", new Tablet("Apple IPad 2th generation"));
+		Cabinet cabinet = new Cabinet();
+		cabinet.put("topShelf", new Phone("Apple Iphone 11"));
+		cabinet.put("middleShelf", new Book("Book: Programming in Java for beginners"));
+	        cabinet.put("bottomShelf", new Tablet("Apple IPad 2th generation"));
 			
 	        
 	        Thing thing = cabinet.whatIsOn("topShelf");
 	        System.out.println(thing.getName());
-		    thing = cabinet.whatIsOn("middleShelf");
-		    System.out.println(thing.getName());
-		    thing = cabinet.whatIsOn("bottomShelf");
-		    System.out.println(thing.getName());
+		thing = cabinet.whatIsOn("middleShelf");
+		System.out.println(thing.getName());
+		thing = cabinet.whatIsOn("bottomShelf");
+		System.out.println(thing.getName());
 	    	
 		    
-		    thing = cabinet.take("topshelf");
+	        thing = cabinet.take("topshelf");
 		    try{
 		    System.out.println(thing.getName());
-	        
-		    }catch(NullPointerException e) {
-	    	System.err.println("There is no object on the top shelf!");
-	    }
+	            }catch(NullPointerException e) {
+	    	          System.err.println("There is no object on the top shelf!");
+	            }
 	   
 	}
 
